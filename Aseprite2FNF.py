ef277b70bcac037322a9cd25d1f183b2614fb423
Lethrial <38376404+Lethrial@users.noreply.github.com>
2022-02-25 18:11:25 +0000

    Now uses less variables

--- Aseprite2FNF.py
+++ Aseprite2FNF.py
@@ -3,24 +3,19 @@ INDENT = '  '
 
 jsonInput = input("Please type the name of the .json you'd like to convert: ")
 xmlName = jsonInput[:-5]+".xml"
-jsonFile = open(jsonInput,"r")
-jsonRead = jsonFile.read()
-jsonPy = json.loads(jsonRead)
-
+jsonFile = open(jsonInput,"r").read()
+jsonPy = json.loads(jsonFile)
 frames = list(jsonPy['frames'].keys())
-frame_len = len(frames)
-image = jsonPy['meta']['image']
 
 with open(xmlName,"a") as xml:
   xml.write('<?xml version="1.0" encoding="utf-8"?>')
-  xml.write('\n<TextureAtlas imagePath="'+image+'">')
-  for f in range(frame_len):
-    frameName = str(frames[f])
-    x = jsonPy['frames'][frameName]['frame']['x']
-    y = jsonPy['frames'][frameName]['frame']['y']
-    w = jsonPy['frames'][frameName]['frame']['w']
-    h = jsonPy['frames'][frameName]['frame']['h']
-    xml.write('\n'+INDENT+'<SubTexture name="'+frameName+'" x="'+str(x)+'" y="'+str(y)+'" width="'+str(w)+'" height="'+str(h)+'"/>')
+  xml.write('\n<TextureAtlas imagePath="'+jsonPy['meta']['image']+'">')
+  for f in range(len(frames)):
+    x = jsonPy['frames'][str(frames[f])]['frame']['x']
+    y = jsonPy['frames'][str(frames[f])]['frame']['y']
+    w = jsonPy['frames'][str(frames[f])]['frame']['w']
+    h = jsonPy['frames'][str(frames[f])]['frame']['h']
+    xml.write('\n'+INDENT+'<SubTexture name="'+str(frames[f])+'" x="'+str(x)+'" y="'+str(y)+'" width="'+str(w)+'" height="'+str(h)+'"/>')
   xml.write('\n</TextureAtlas>')
 
 print("Conversion complete! Your output file is "+xmlName+".")
