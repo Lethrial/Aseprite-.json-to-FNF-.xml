aac8f6157af70341c63c49b9c5a1afb6ae5dd76d
Lethrial <38376404+Lethrial@users.noreply.github.com>
2022-05-29 14:27:23 +0100

    Farewell, range(len()) o7
    - Removed the range(len()) idiom
    - Frame data is now generated with f-strings instead of concatenation
    - Added credits to this GitHub page (it'd be nice if you kept it in the file if you release a mod using this tool)

--- Aseprite2FNF.py
+++ Aseprite2FNF.py
@@ -7,15 +7,16 @@ jsonFile = open(jsonInput,"r").read()
 jsonPy = json.loads(jsonFile)
 frames = list(jsonPy['frames'].keys())
 
-with open(xmlName,"a") as xml:
+with open(xmlName,"w") as xml:
   xml.write('<?xml version="1.0" encoding="utf-8"?>')
   xml.write('\n<TextureAtlas imagePath="'+jsonPy['meta']['image']+'">')
-  for f in range(len(frames)):
-    x = jsonPy['frames'][str(frames[f])]['frame']['x']
-    y = jsonPy['frames'][str(frames[f])]['frame']['y']
-    w = jsonPy['frames'][str(frames[f])]['frame']['w']
-    h = jsonPy['frames'][str(frames[f])]['frame']['h']
-    xml.write('\n'+INDENT+'<SubTexture name="'+str(frames[f])+'" x="'+str(x)+'" y="'+str(y)+'" width="'+str(w)+'" height="'+str(h)+'"/>')
+  xml.write(f'\n{INDENT}<!-- Generated using Aseprite .json to FNF .xml: https://github.com/Lethrial/Aseprite-.json-to-FNF-.xml -->')
+  for f in frames:
+    x = jsonPy['frames'][str(f)]['frame']['x']
+    y = jsonPy['frames'][str(f)]['frame']['y']
+    w = jsonPy['frames'][str(f)]['frame']['w']
+    h = jsonPy['frames'][str(f)]['frame']['h']
+    xml.write(f'\n{INDENT}<SubTexture name="{str(f)}" x="{str(x)}" y="{str(y)}" width="{str(w)}" height="{str(h)}"/>')
   xml.write('\n</TextureAtlas>')
 
 print("Conversion complete! Your output file is "+xmlName+".")
